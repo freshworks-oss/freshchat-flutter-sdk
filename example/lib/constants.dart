a657f5bc2fd3d922f186afc5c1d61e708726f43a
karthikeyan-pandu <karthikeyan.pandu@freshworks.com>
2026-02-16 16:07:12 +0530

    Removing the APPID, APPKEY credentials used for testing

--- example/lib/constants.dart
+++ example/lib/constants.dart
@@ -1,3 +1,3 @@
-const APPID = "abeaa0e6-b2de-4852-9a75-90d91e64294f";
-const APPKEY = "eb7a2fa0-edeb-468e-bbcd-3459103f30c0";
-const DOMAIN = "msdk.au.freshchat.com";
+const APPID = "";
+const APPKEY = "";
+const DOMAIN = "";
